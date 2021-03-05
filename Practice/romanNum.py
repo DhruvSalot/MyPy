5fe9bce76bbe5fd8b13dde5974e54567bd97ff65
Dhruv Salot <salot8898309999@outlook.com>
2021-03-05 15:10:48 +0530

    Update romanNum.py

--- Practice/romanNum.py
+++ Practice/romanNum.py
@@ -1,35 +1,35 @@
-# making program that converts given number to roman number form (numbers in range(1000))
-keyNum = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+#making program that converts given number to roman number form (numbers in range(1000))
 
-roNumDict = { 1    : "I" ,
-              4    : "IV",
-              5    : "V" ,
-              9    : "IX",
-              10   : "X" ,
-              40   : "XL",
-              50   : "L" ,
-              90   : "XC",
-              100  : "C" ,
-              400  : "CD",
-              500  : "D" ,
-              900  : "CM",
-              1000 : "M" }
+keyNum = [1, 4, 5, 9, 10, 40, 50, 90, 100,
+           400, 500, 900, 1000]
 
-inputNum = int(input("> "))
+roNumDict = {1   : "I",
+             4   : "IV",
+             5   : "V",
+             9   : "IX",
+             10  : "X",
+             40  : "XL",
+             50  : "L",
+             90  : "XC",
+             100 : "C",
+             400 : "CD",
+             500 : "D",
+             900 : "CM",
+             1000 : "M" }
 
-# defining a function to return keyNum just less than inputNum
-def romanNum(int, list, dict):
+inputNum = (input("> "))
+
+def romanNum(str, list, dict):
     for i in range(len(list)):
-        if list[i] - int > 0:
+        if list[i] - int(str) > 0:
              return(list[i-1])
              break
 
-# Making list of numbers inputNum can break to ie 879 = 500+100+100+100+50+10+10+9 
 myList = []
-number = inputNum
+number = int(inputNum)
 while number > 0 :
-    myList   += [romanNum(number, keyNum, roNumDict)]
-    inputNum -=  romanNum(number, keyNum, roNumDict)
+    myList +=[romanNum(number, keyNum, roNumDict)]
+    number -= romanNum(number, keyNum, roNumDict)
 
 finalStr = ""
 for i in range(len(myList)):
